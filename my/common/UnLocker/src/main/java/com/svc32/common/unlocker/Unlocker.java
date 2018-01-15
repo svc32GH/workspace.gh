d9e2dfec70f20441db56aa0ddc85e68996a17751
Sergey Chudakov <svc32@yahoo.com>
2018-01-15 23:10:23 +0200

    Commit template code of UnLocker PC-2

--- my/common/UnLocker/src/main/java/com/svc32/common/unlocker/Unlocker.java
+++ my/common/UnLocker/src/main/java/com/svc32/common/unlocker/Unlocker.java
@@ -6,12 +6,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Unlocker extends JFrame {
+    private static final int x = 4200;
+    private static final int y = 655;
+//    private static final int x = 1370;
+//    private static final int y = 15;
+    private static final int width = 300;
+    private static final int height = 300;
 
     public JFrame thisFrame;
 
     public Unlocker() {
         super("Unlock Monitor");
-        this.setBounds(4200, 655, 300, 300);
+        this.setBounds(x, y, width, height);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         thisFrame = this;
@@ -25,9 +31,23 @@ public class Unlocker extends JFrame {
                               }
         );
 
+        JPanel pnl = new JPanel();
+        pnl.setLayout(new BorderLayout(5, 5));
+        pnl.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+        final DefaultListModel listModel = new DefaultListModel();
+        JList list = new JList(listModel);
+        pnl.add(new JScrollPane(list), BorderLayout.WEST);
+        for (int i = 0; i < 25; i++) {
+            listModel.addElement("Элемент списка " + i);
+        }
+
+        pnl.add(list);
+
         Container cn = this.getContentPane();
         cn.setLayout(new FlowLayout());
-        cn.add(btn);
+//        cn.add(btn);
+        cn.add(pnl);
 
 
         //        this.getBounds()
