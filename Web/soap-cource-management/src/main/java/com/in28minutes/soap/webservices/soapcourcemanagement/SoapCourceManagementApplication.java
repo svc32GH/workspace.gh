6b066a62f8a56b77f52d64accb390764e503b019
Sergey Chudakov <svc32@yahoo.com>
2018-05-08 18:45:52 +0300

    'in28Minutes'-SpringMVC lessons: commit #9: start implementing SOAP Web Services

---
+++ Web/soap-cource-management/src/main/java/com/in28minutes/soap/webservices/soapcourcemanagement/SoapCourceManagementApplication.java
@@ -0,0 +1,12 @@
+package com.in28minutes.soap.webservices.soapcourcemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCourceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCourceManagementApplication.class, args);
+	}
+}
